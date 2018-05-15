76b94959b72e73096d99c5961b115ff9eb20f9d5
cyril.quere <cyril.quere@gmail.com>
2018-05-15 10:03:49 +0200

    ajout composant detail-collegue

---
+++ src/app/detail-collegue/detail-collegue.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-collegue',
+  templateUrl: './detail-collegue.component.html',
+  styleUrls: ['./detail-collegue.component.css']
+})
+export class DetailCollegueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
